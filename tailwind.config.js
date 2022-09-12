6ca56973747ac60abf56f09d63a427cd3da2f9ee
Andrés Perdomo <alvarado0925@gmail.com>
2022-09-12 16:45:07 -0500

    Make necessary adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./public/index.html', './src/**/*.{html,js}'],
   theme: {
     extend: {},
   },
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  }
+  plugins: [],
 }
